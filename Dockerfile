a1dc3eb6bf088182c173616f7939706da532a8ed
abyssal <abyssal512@gmail.com>
2019-10-12 18:20:27 +1300

    Add Abyss.Host web server project, switch Dockerfile to use it

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM mcr.microsoft.com/dotnet/core/sdk:3.0 AS build
 ARG Configuration=Release
 WORKDIR /build
 COPY . ./
-RUN dotnet restore src/Abyss.Console/Abyss.Console.csproj
-RUN dotnet build src/Abyss.Console/Abyss.Console.csproj -c $Configuration -o /app
+RUN dotnet restore src/Abyss.Host/Abyss.Host.csproj
+RUN dotnet build src/Abyss.Host/Abyss.Host.csproj -c $Configuration -o /app
 
-FROM mcr.microsoft.com/dotnet/core/runtime:3.0 AS runtime
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.0 AS runtime
 WORKDIR /app
 COPY --from=build /app .
 
-ENTRYPOINT ["dotnet", "Abyss.Console.dll", "/data"]
+ENTRYPOINT ["dotnet", "Abyss.Host.dll", "/data"]
---
+++ src/Abyss.Host/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Abyss.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var contentRoot = args.Length > 0 ? args[0] : AppDomain.CurrentDomain.BaseDirectory;
+
+            return Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+                .UseContentRoot(contentRoot)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+                    config.AddJsonFile("abyss.json", false, true);
+                    config.AddJsonFile($"abyss.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        }
+    }
+}
