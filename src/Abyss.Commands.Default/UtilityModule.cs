e470d718ce61fac2e697af62953e272efa1299f4
Abyssal <abyssal512@gmail.com>
2019-10-24 18:17:11 +1300

    Updated time libraries and added exception handling

--- src/Abyss.Commands.Default/UtilityModule.cs
+++ src/Abyss.Commands.Default/UtilityModule.cs
@@ -1,5 +1,6 @@
 using Discord;
 using Discord.WebSocket;
+using HumanDateParser;
 using Qmmands;
 using System;
 using System.Diagnostics;
@@ -17,16 +18,28 @@ namespace Abyss.Commands.Default
         [Description("Displays time.")]
         public Task<ActionResult> Command_TimeAsync([Name("Text")] [Remainder] string text)
         {
-            return Ok($"`{text}` => {HumanDateParser.HumanDateParser.Parse(text)}");
+            try
+            {
+                return Ok($"`{text}` => {HumanDateParser.HumanDateParser.Parse(text)}");
+            } catch (ParseException pe)
+            {
+                return BadRequest($"Error during parsing: " + pe.Message);
+            }
         }
 
         [Command("timed")]
         [Description("Displays time, in debug mode.")]
         public Task<ActionResult> Command_TimeDetailedAsync([Name("Text")] [Remainder] string text)
         {
-            var res = HumanDateParser.HumanDateParser.ParseDetailed(text);
-            return Ok($"`{text}` => {res.Result.ToString()}\n" +
+            try {
+                var res = HumanDateParser.HumanDateParser.ParseDetailed(text);
+                return Ok($"`{text}` => {res.Result.ToString()}\n" +
                 $"**Tokens:** {string.Join(", ", res.Tokens.Select(c => Format.Code(c.GetType().Name)))}");
+            }
+            catch (ParseException pe)
+            {
+                return BadRequest($"Error during parsing: " + pe.Message);
+            }
         }
 
         [Command("ping")]
