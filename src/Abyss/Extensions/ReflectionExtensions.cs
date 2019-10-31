319b1c1648ae914e1b5409a379d68a2e08e5cb3a
Abyssal <abyssal512@gmail.com>
2019-10-31 22:12:39 +1300

    Update ReflectionExtensions.cs

--- src/Abyss/Extensions/ReflectionExtensions.cs
+++ src/Abyss/Extensions/ReflectionExtensions.cs
@@ -11,7 +11,7 @@ namespace Abyss
         ///     Attempts to find the informational version of an assembly, otherwise defaults to the registered version.
         /// </summary>
         /// <param name="assembly">The assembly to query version for.</param>
-        /// <returns>The best available format the version of the provided assembly.</returns>
+        /// <returns>The best available format of the version of the provided assembly.</returns>
         public static string GetVersion(this Assembly assembly)
             => assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? assembly.GetName().Version!.ToString();
     }
