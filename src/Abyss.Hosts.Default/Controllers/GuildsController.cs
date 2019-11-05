92988ee4ae8a5575e04186c78eb41e28eb6e557f
abyssal <abyssal512@gmail.com>
2019-11-05 23:11:19 +1300

    Add member count endpoint

---
+++ src/Abyss.Hosts.Default/Controllers/GuildsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abyss.Hosts.Default.Controllers
+{
+    [Controller]
+    [Route("api/guilds")]
+    public class GuildsController : Controller
+    {
+        private readonly AbyssBot _bot;
+
+        public GuildsController(AbyssBot bot)
+        {
+            _bot = bot;
+        }
+
+        [HttpGet("{id}/membercount")]
+        public IActionResult GetMemberCountAsync(ulong guild)
+        {
+            return Ok(new { member_count = _bot.Guilds[guild].MemberCount });
+        }
+    }
+}
