9c98db803b1ef438c2d3f1f6ba5d31b0b5214421
abyssal <jackson@rakena.co.nz>
2020-12-20 14:37:43 +1300

    Fix JSON libraries

--- src/com/abyssaldev/abyss/interactions/Interaction.kt
+++ src/com/abyssaldev/abyss/interactions/Interaction.kt
@@ -1,5 +1,8 @@
 package com.abyssaldev.abyss.interactions
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 class Interaction {
     lateinit var id: String
     var type: Int? = null
