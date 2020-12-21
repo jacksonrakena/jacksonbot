0441fb453892c57ddeaddbac66ad3039122dfb5c
abyssal <jackson@rakena.co.nz>
2020-12-21 20:38:28 +1300

    Prepare info command for production

--- src/main/kotlin/com/abyssaldev/abyss/interactions/commands/InfoCommand.kt
+++ src/main/kotlin/com/abyssaldev/abyss/interactions/commands/InfoCommand.kt
@@ -10,24 +10,15 @@ class InfoCommand : InteractionCommand() {
     override val name = "info"
     override val description = "Shows information about a user, or a role."
     override val options: Array<InteractionCommandOption> = arrayOf(
-        InteractionSubcommandSimple("user", description = "View information about a user.", options = arrayOf(
+        InteractionSubcommandSimple("user", description = "Finds information about a user.", options = arrayOf(
             InteractionCommandArgument("user", description = "The user to view information about.", type = InteractionCommandArgumentType.User, isRequired = true)
         )) {
             return@InteractionSubcommandSimple respond("Info about user " + this.arguments[0].value)
         },
-        InteractionSubcommandSimple("role", description = "View information about a role.", options = arrayOf(
+        InteractionSubcommandSimple("role", description = "Finds information about a role.", options = arrayOf(
             InteractionCommandArgument("role", description = "The role to view information about.", type = InteractionCommandArgumentType.Role, isRequired = true)
         )) {
             return@InteractionSubcommandSimple respond("Info about role " + this.arguments[0].value)
         }
     )
-    /*override val options: Array<InteractionCommandOption> = arrayOf(
-        InteractionSubcommandGroup("user", description = "View information about a user.", subcommands = arrayOf(
-            InteractionSubcommandSimple("get", description = "View information about a user.", options = arrayOf(
-                InteractionCommandArgument("user", description = "The user to view information about.", type = InteractionCommandArgumentType.User, isRequired = true)
-            )) {
-                return@InteractionSubcommandSimple respond("Info about user " + this.arguments[0].value)
-            }))
-    )*/
-    override val guildLock = 385902350432206849
 }
