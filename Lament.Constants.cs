a8ee0fd10ce2cb4351e88738104af55507b68b20
abyssal <jackson@rakena.co.nz>
2021-02-22 23:58:57 +1300

    Add runtime pipeline

---
+++ Lament.Constants.cs
@@ -0,0 +1,11 @@
+namespace Lament
+{
+    public static class Constants
+    {
+        public const string ENVIRONMENT_VARIABLE_PREFIX = "LAMENT_";
+        public const string ENVIRONMENT_VARNAME = ENVIRONMENT_VARIABLE_PREFIX + "ENVIRONMENT";
+        public const string DEFAULT_RUNTIME_ENVIRONMENT = "Development";
+
+        public const string CONFIGURATION_FILENAME = "lament.appsettings.json";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace Lament
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Lament runtime starting");
+                BuildLamentHost(args).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHost BuildLamentHost(string[] runtimeArgs)
+        {
+            var hostBuilder = new HostBuilder();
+            hostBuilder.UseSystemd();
+            hostBuilder.UseSerilog();
+            hostBuilder.ConfigureAppConfiguration(appConfigure =>
+            {
+                appConfigure.AddJsonFile(Constants.CONFIGURATION_FILENAME);
+                appConfigure.AddEnvironmentVariables(Constants.ENVIRONMENT_VARIABLE_PREFIX);
+            });
+            var environment = Environment.GetEnvironmentVariable(Constants.ENVIRONMENT_VARNAME);
+            if (environment == null)
+            {
+                Log.Error("{1} variable not set. Defaulting to {0}", Constants.DEFAULT_RUNTIME_ENVIRONMENT, Constants.ENVIRONMENT_VARNAME);
+                environment = Constants.DEFAULT_RUNTIME_ENVIRONMENT;
+            }
+            hostBuilder.UseEnvironment(environment);
+            hostBuilder.ConfigureServices(ConfigureServices);
+            
+            return hostBuilder.Build();
+        }
+
+        private static void ConfigureServices(HostBuilderContext builderContext, IServiceCollection serviceCollection)
+        {
+            
         }
     }
 }
